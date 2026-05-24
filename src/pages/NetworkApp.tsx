d07e981f21522a7c9533c457267a2821ba971f4e
its-slow <hsn085185@gmail.com>
2026-05-24 03:45:10 +0300

    Update NetworkApp.tsx

--- src/pages/NetworkApp.tsx
+++ src/pages/NetworkApp.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import { toast } from "sonner";
-import { useClerk, useUser } from "@clerk/react";
 import {
   DeviceEntry,
   loadFromFirebase,
